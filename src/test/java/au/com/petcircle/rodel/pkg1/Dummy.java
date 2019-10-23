93610ac8454a04406ff8c770ef7576f13df22b21
rodelcalasagsag-pc <rodel.calasagsag@pawsforlife.com.au>
2019-10-24 07:34:15 +0800

    testing junit

--- src/test/java/au/com/petcircle/rodel/pkg1/Dummy.java
+++ src/test/java/au/com/petcircle/rodel/pkg1/Dummy.java
@@ -1,14 +1,13 @@
 package au.com.petcircle.rodel.pkg1;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
-import java.util.Random;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Dummy {
 
-    @Test()
+    @Test
     public void test1() {
-        Assert.assertTrue(true, "MY ASSERTION FAILED!!");
+        Assert.assertTrue("FORCED FAILURE", Boolean.TRUE);
     }
 }
